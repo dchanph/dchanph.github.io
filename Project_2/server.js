4c011178247099841f0f6f0b48b433a9132c932b
Doreen Chan <doreenchan@Doreens-MacBook-Pro.local>
2019-11-09 20:31:38 +0800

    added new route

--- Project_2/server.js
+++ Project_2/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+//Create New route
+app.get("/events/new", (req, res) => {
+  res.send("new");
+});
+
 app.listen(3000, () => {
   console.log("listening");
 });
