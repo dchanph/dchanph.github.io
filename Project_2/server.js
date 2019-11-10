adea6bcdd16e11523a6df5c33b1723d7590387fe
Doreen Chan <doreenchan@Doreens-MacBook-Pro.local>
2019-11-10 08:45:45 +0800

    initialize other dependencies

--- Project_2/server.js
+++ Project_2/server.js
@@ -1,11 +1,23 @@
+//___________________
+//Dependencies
+//___________________
 const express = require("express");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
 const app = express();
+
+//___________________
+//Port
+//___________________
+const PORT = process.env.PORT || 3000;
 
 //Create New route
 app.get("/events/new", (req, res) => {
   res.render("new.ejs");
 });
 
-app.listen(3000, () => {
-  console.log("listening");
-});
+//___________________
+//Listener
+//___________________
+app.listen(PORT, () => console.log("Awesome Events!", PORT));
