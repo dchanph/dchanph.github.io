c3dd0c954e1563436f56162b3ac414e7fe8fd6fa
doreenchan <dchanph@gmail.com>
2019-11-19 14:59:43 +0800

    added sessions

--- Project_2/controllers/users.js
+++ Project_2/controllers/users.js
@@ -1,8 +1,27 @@
+const bcrypt = require("bcrypt");
 const express = require("express");
-const router = express.Router();
+const users = express.Router();
+const User = require("../models/users.js");
 
-router.get("/new", (req, res) => {
+users.get("/new", (req, res) => {
   res.render("users/new.ejs");
 });
 
-module.exports = router;
+// users/new form submit
+
+users.post("/", (req, res) => {
+  req.body.password = bcrypt.hashSync(
+    req.body.password,
+    bcrypt.genSaltSync(10)
+  );
+  User.create(req.body, (err, createdUser) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(createdUser);
+    // once user is created redirect back to 'index page'
+    res.redirect("/");
+  });
+});
+
+module.exports = users;
