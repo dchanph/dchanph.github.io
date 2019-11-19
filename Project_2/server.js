f838b3f7ca706ed67ef221105a2ad2f2b5f2148b
doreenchan <dchanph@gmail.com>
2019-11-19 15:37:54 +0800

    added sessions

--- Project_2/server.js
+++ Project_2/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const session = require("express-session");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 
@@ -12,9 +13,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static("public"));
 
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
 // Use Controllers and Routes
 app.use("/events", eventsController);
 app.use("/users", usersController);
+app.use("/users", userController);
+app.use("/sessions", sessionsController);
 
 //connect to database
 mongoose.connect("mongodb://localhost:27017/eventsProject", {
