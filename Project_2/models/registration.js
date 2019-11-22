4d273d561dd9c8fbd2e46318a0909c796408344d
doreenchan <dchanph@gmail.com>
2019-11-22 15:21:53 +0800

    delete button is working now

--- Project_2/models/registration.js
+++ Project_2/models/registration.js
@@ -3,11 +3,12 @@ const Schema = mongoose.Schema;
 
 const registrationSchema = mongoose.Schema({
   user: { type: Schema.Types.ObjectId, ref: "User" },
+  user: String,
   first_name: String,
+  last_name: String,
   dept: String,
   event: { type: Schema.Types.ObjectId, ref: "Event" },
-  ticketsQty: Number,
-  totalCost: Number
+  ticketsQty: Number
 });
 
 const Registration = mongoose.model("Registration", registrationSchema);
